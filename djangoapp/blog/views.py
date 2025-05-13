90ea60c0fcfd58338c19fa3475aa63f7a14adb8c
Juliane Mafissoni <julianemafissoni@hotmail.com>
2025-05-13 10:16:54 -0300

    atualizando o git

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,7 +1,9 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
-from blog.models import Post
+from django.db.models import Q
+from blog.models import Post, Page
 
+PER_PAGE = 9
 # Create your views here.
 def index(request):
     posts = (Post.objects.get_published())
@@ -52,11 +54,18 @@ def category(request, slug):
  
 
 def page(request, slug):
+    page = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+
     return render(
         request,
         'blog/pages/page.html',
         {
-             # 'page_obj': page_obj,
+             'page': page,
         }
     )
 
@@ -71,3 +80,42 @@ def post(request, slug):
         }
         
     )
+
+def tag(request, slug):
+    posts = (Post.objects.get_published())\
+        .filter(tags__slug=slug)
+
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+         request,
+         'blog/pages/index.html',
+         {
+             'page_obj': page_obj,
+         }
+     )
+ 
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+
+    posts = (
+        Post.objects.get_published()
+        .filter(
+            Q(title__icontains=search_value) |
+            Q(excerpt__icontains=search_value) |
+            Q(content__icontains=search_value)
+        )[:PER_PAGE]
+    )
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
+        }
+    )
+
+ 
